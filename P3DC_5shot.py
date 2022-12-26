72d4ab441c372901506ea128c9d21d277775b27f
breakaway7 <119171100+breakaway7@users.noreply.github.com>
2022-12-26 23:24:25 +0800

    Add files via upload

---
+++ P3DC_5shot.py
@@ -0,0 +1,126 @@
+import pickle
+import numpy as np
+from tqdm import tqdm
+import torch
+import torch.nn.functional as F
+
+
+def compute_confidence_interval(data):
+    """
+    Compute 95% confidence interval
+    :param data: An array of mean accuracy (or mAP) across a number of sampled episodes.
+    :return: the 95% confidence interval for this data.
+    """
+    a = 1.0 * np.array(data)
+    m = np.mean(a)
+    std = np.std(a)
+    pm = 1.96 * (std / np.sqrt(len(a)))
+    return m, pm
+
+
+def knn_matrix(d, k):
+    sort = np.argsort(d, axis=1)
+    nn = sort[:, -k:]
+    dd = np.zeros((n_ways * n_shot, R))
+    for i in range(nn.shape[0]):
+        for j in range(nn.shape[1]):
+            dd[i, j] = d[i, nn[i, j]]
+    dk = F.softmax(torch.Tensor(dd), dim=1)
+    return dk, nn
+
+
+
+if __name__ == '__main__':
+    # ---- data loading
+    dataset = 'miniImagenet'
+    n_shot = 5
+    n_ways = 5
+    n_queries = 15
+    n_runs = 2000
+    n_lsamples = n_ways * n_shot
+    n_usamples = n_ways * n_queries
+    n_samples = n_lsamples + n_usamples
+
+
+    import FSLTask
+    cfg = {'shot': n_shot, 'ways': n_ways, 'queries': n_queries}
+    FSLTask.loadDataSet(dataset)
+    FSLTask.setRandomStates(cfg)
+    ndatas = FSLTask.GenerateRunSet(end=n_runs, cfg=cfg)
+    ndatas = ndatas.permute(0, 2, 1, 3).reshape(n_runs, n_samples, -1)
+    labels = torch.arange(n_ways).view(1, 1, n_ways).expand(n_runs, n_shot + n_queries, 5).clone().view(n_runs,
+                                                                                                        n_samples)
+
+    # ---- Base class statistics
+    base_means = []
+    base_features_path = "./checkpoints/%s/WideResNet28_10_S2M2_R/last/base_features.plk" % dataset
+
+    #加载特征
+    with open(base_features_path, 'rb') as f:
+        data = pickle.load(f)
+        for key in data.keys():
+            feature = np.array(data[key])
+            mean = np.mean(feature, axis=0)
+            base_means.append(mean)
+
+
+    #base_means 是列表
+    base_prototype = np.array(base_means)
+    # ---- classification for each task
+    acc_list = []
+    account = []
+    print('Start classification for %d tasks...'%(n_runs))
+    for i in tqdm(range(n_runs)):
+        support_data = ndatas[i][:n_lsamples].numpy()
+        support_label = labels[i][:n_lsamples].numpy()
+        query_data = ndatas[i][n_lsamples:].numpy()
+        query_label = labels[i][n_lsamples:].numpy()
+
+        support_data_norm = support_data / np.linalg.norm(support_data, 2, 1)[:, None]
+
+        beta = 0.5
+        support_data = np.power(support_data[:, ], beta)
+        R = 5
+        # sample-level
+        sw = np.dot(support_data, base_prototype.transpose())
+        sbase, rn = knn_matrix(sw, R)
+        sbase = np.expand_dims(sbase, 2).transpose(0, 2, 1)
+        rneighbor = base_prototype[rn]
+        sr = torch.bmm(torch.Tensor(sbase), torch.Tensor(rneighbor))
+        sr = np.squeeze(sr.numpy())
+        r = sr + support_data
+        r = r / np.linalg.norm(r, 2, 1)[:, None]
+
+        # task-level
+        tw = np.dot(support_data, base_prototype.transpose())
+        sort = np.argsort(tw, axis=1)
+        index = sort[:, -R:]
+        tneighbor = np.unique(index.ravel())
+        tbase = base_prototype[tneighbor]
+        ts = np.dot(support_data, tbase.transpose())
+        ts = F.softmax(torch.Tensor(ts), dim=1)
+        t = np.dot(ts, tbase) + support_data
+        t = t / np.linalg.norm(t, 2, 1)[:, None]
+
+        a = 0.0
+        b = 0.4
+        c = 1 - a - b
+        proto = c * support_data_norm + a * r + b * t
+
+        qpw = np.dot(query_data, proto.transpose()).reshape(-1, n_shot, n_ways)
+        qpw = F.softmax(torch.Tensor(qpw), dim=1)
+        qpw = torch.unsqueeze(qpw.view(n_usamples,-1), 2).repeat(1, 1, query_data.shape[1])
+        proto = torch.unsqueeze(torch.tensor(proto), 0).repeat(n_usamples, 1, 1)
+        proto = torch.mul(proto, qpw)
+        proto = torch.sum(proto.reshape(-1, n_shot, n_ways, query_data.shape[1]), axis=1)
+        proto = proto / np.linalg.norm(proto, 2, 2)[:,:, None]
+
+        query_data = torch.unsqueeze(torch.tensor(query_data), 2)
+        query_data = query_data / np.linalg.norm(query_data, 2, 1)[:, None]
+        predicts = torch.bmm( proto, query_data)
+        predicts = np.squeeze(np.array(np.argmax(predicts, axis=1)))
+        acc = np.mean(predicts== query_label)
+        acc_list.append(acc)
+
+    mean, confidence = compute_confidence_interval(acc_list)
+    print('%s %d way %d shot  ACC : %f %f'%(dataset,n_ways,n_shot,float(mean), float(confidence)))
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# P3DC-Shot
-P3DC-Shot: Prior-Driven Discrete Data Calibration for Nearest-Neighbor Few-Shot Classification
+# P3DC-Shot: Prior-Driven Discrete Data Calibration for Nearest-Neighbor Few-Shot Classification
+
+
+
+
+## Backbone Training
+
+We use the same backbone network and training strategies as 'S2M2_R'. Please refer to https://github.com/nupurkmr9/S2M2_fewshot for the backbone training.
+
+
+## Extract and save features
+
+After training the backbone as 'S2M2_R', extract features as below:
+
+- Create an empty 'checkpoints' directory.
+
+- Run:
+```save_features
+python save_plk.py --dataset [miniImagenet/CUB] 
+```
+### Or you can directly download the extracted features/pretrained models from the link:
+https://drive.google.com/drive/folders/1IjqOYLRH0OwkMZo8Tp4EG02ltDppi61n?usp=sharing
+
+
+After downloading the extracted features, please adjust your file path according to the code.
+
+
+## Evaluate our distribution calibration
+
+To evaluate our P3DC-Shot method, run:
+
+```eval
+python P3DC.py for 1-shot
+python P3DC_5shot.py for 5-shot
+```
+
+
+
+## Reference
+
+[Charting the Right Manifold: Manifold Mixup for Few-shot Learning](https://arxiv.org/pdf/1907.12087v3.pdf)
+
+[https://github.com/nupurkmr9/S2M2_fewshot](https://github.com/nupurkmr9/S2M2_fewshot)
+
+[Free Lunch for Few-shot Learning: Distribution Calibration](https://arxiv.org/abs/2101.06395)
+
+[https://github.com/ShuoYang-1998/Few_Shot_Distribution_Calibration](https://github.com/ShuoYang-1998/Few_Shot_Distribution_Calibration)
+
+
+
